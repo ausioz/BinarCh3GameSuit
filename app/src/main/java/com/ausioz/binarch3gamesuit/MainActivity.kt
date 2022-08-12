37b761558a9615cfb83a393b6820d578b489084d
ausioz <oz.ausi@gmail.com>
2022-08-12 08:43:24 +0700

    create game mode dummy

--- app/src/main/java/com/ausioz/binarch3gamesuit/MainActivity.kt
+++ app/src/main/java/com/ausioz/binarch3gamesuit/MainActivity.kt
@@ -10,12 +10,11 @@ import com.ausioz.binarch3gamesuit.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private val textTitle = (R.string.app_title_text)
-
-    private val pilihanSuit = arrayOf("Batu", "Gunting", "Kertas")
-    private var status: String? = null
-    private var pemain1: String? = null
-    private var pemain2: String? = null
     private var _binding: ActivityMainBinding? = null
+    private val pilihanSuit = arrayOf("Batu", "Gunting", "Kertas")
+    private var pemain1:String? = null
+    private var pemain2:String? = null
+    private var status:String? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,37 +26,97 @@ class MainActivity : AppCompatActivity() {
         _binding?.textTitle?.text = (getText(textTitle))
         _binding?.textTerminal?.text = "VS"
 
+
         // 0: Batu      1: Gunting      2:Kertas
         _binding?.playerBtnBatu?.setOnClickListener {
-            refreshPick()
+            refreshPickP1()
             activePickBatu()
             pemain1 = pilihanSuit[0]
-            Log.d("Pemain 1 Input", "$pemain1")
-            doRule()
+            Log.d("Pemain 1 Input", pemain1!!)
         }
         _binding?.playerBtnGunting?.setOnClickListener {
-            refreshPick()
+            refreshPickP1()
             activePickGunting()
             pemain1 = pilihanSuit[1]
-            Log.d("Pemain 1 Input", "$pemain1")
-            doRule()
+            Log.d("Pemain 1 Input", pemain1!!)
         }
         _binding?.playerBtnKertas?.setOnClickListener {
-            refreshPick()
+            refreshPickP1()
             activePickKertas()
             pemain1 = pilihanSuit[2]
-            Log.d("Pemain 1 Input", "$pemain1")
+            Log.d("Pemain 1 Input", pemain1!!)
+        }
+
+        fun player2Mode(){
+            _binding?.comBtnBatu?.setOnClickListener {
+                refreshPickP2()
+                activePickBatuP2()
+                pemain2 = pilihanSuit[0]
+                Log.d("Pemain 2 Input", pemain2!!)
+            }
+            _binding?.comBtnGunting?.setOnClickListener {
+                refreshPickP2()
+                activePickGuntingP2()
+                pemain2 = pilihanSuit[1]
+                Log.d("Pemain 2 Input", pemain2!!)
+            }
+            _binding?.comBtnKertas?.setOnClickListener {
+                refreshPickP2()
+                activePickKertasP2()
+                pemain2 = pilihanSuit[2]
+                Log.d("Pemain 2 Input", pemain2!!)
+            }
+
+        }
+        _binding?.modeP2?.setOnClickListener {
+            player2Mode()
+            enablePickP1()
+            refreshPickP1()
+            refreshPickP2()
             doRule()
+            _binding?.textTerminal?.text = "VS"
+            _binding?.btnLockPick?.setOnClickListener {
+                disablePickP1()
+                refreshPickP1()
+                inactivePickP1()
+                enablePickP2()
+            }
+            _binding?.btnLockPick2?.setOnClickListener {
+                disablePickP2()
+                refreshPickP2()
+                inactivePickP2()
+                Log.d("Pemain 2 Input lock", pemain2!!)
+            }
+            _binding?.start?.setOnClickListener {
+
+                Log.d("Pemain 2 Input lock", pemain2!!)
+                playTerminalP2mode()
+            }
         }
-        _binding?.btnLockPick?.setOnClickListener {
-            playTerminal()
-            disablePick()
+
+        _binding?.modeCom?.setOnClickListener{
+            enablePickP1()
+            refreshPickP1()
+            refreshPickP2()
+            _binding?.textTerminal?.text = "VS"
+            pemain2 = pilihanSuit.random()
+            _binding?.btnLockPick?.setOnClickListener {
+                disablePickP1()
+            }
+            _binding?.start?.setOnClickListener {
+                doRule()
+                playTerminalP1mode()
+            }
         }
+
         _binding?.btnRefresh?.setOnClickListener {
-            enablePick()
-            refreshPick()
+            enablePickP1()
+            enablePickP2()
+            refreshPickP1()
+            refreshPickP2()
             _binding?.textTerminal?.text = "VS"
         }
+
     }
 
     private fun doRule() {
@@ -74,7 +133,7 @@ class MainActivity : AppCompatActivity() {
             pemain1 == pilihanSuit[1] && pemain2 == pilihanSuit[0] ||
             pemain1 == pilihanSuit[2] && pemain2 == pilihanSuit[1]
         ) {
-            "COM MENANG!"
+            "Pemain 2 MENANG!"
         } else {
             "DRAW!"
         }
@@ -87,6 +146,13 @@ class MainActivity : AppCompatActivity() {
         _binding?.btnLockPick?.isVisible = true
         _binding?.btnLockPick?.text = "Kunci Pilihan\nBatu"
     }
+    private fun activePickBatuP2() {
+        _binding?.comBtnBatu?.setBackgroundColor(resources.getColor(R.color.active))
+        _binding?.comBtnGunting?.setColorFilter(Color.LTGRAY)
+        _binding?.comBtnKertas?.setColorFilter(Color.LTGRAY)
+        _binding?.btnLockPick2?.isVisible = true
+        _binding?.btnLockPick2?.text = "Kunci Pilihan\nBatu"
+    }
 
     private fun activePickGunting() {
         _binding?.playerBtnBatu?.setColorFilter(Color.LTGRAY)
@@ -94,6 +160,13 @@ class MainActivity : AppCompatActivity() {
         _binding?.playerBtnKertas?.setColorFilter(Color.LTGRAY)
         _binding?.btnLockPick?.isVisible = true
         _binding?.btnLockPick?.text = "Kunci Pilihan\nGunting"
+    }
+    private fun activePickGuntingP2() {
+        _binding?.comBtnBatu?.setColorFilter(Color.LTGRAY)
+        _binding?.comBtnGunting?.setBackgroundColor(resources.getColor(R.color.active))
+        _binding?.comBtnKertas?.setColorFilter(Color.LTGRAY)
+        _binding?.btnLockPick2?.isVisible = true
+        _binding?.btnLockPick2?.text = "Kunci Pilihan\nGunting"
     }
 
     private fun activePickKertas() {
@@ -104,43 +177,90 @@ class MainActivity : AppCompatActivity() {
         _binding?.btnLockPick?.text = "Kunci Pilihan\nKertas"
     }
 
-    private fun inactivePickCom() {
+    private fun activePickKertasP2() {
+        _binding?.comBtnBatu?.setColorFilter(Color.LTGRAY)
+        _binding?.comBtnGunting?.setColorFilter(Color.LTGRAY)
+        _binding?.comBtnKertas?.setBackgroundColor(resources.getColor(R.color.active))
+        _binding?.btnLockPick2?.isVisible = true
+        _binding?.btnLockPick2?.text = "Kunci Pilihan\nKertas"
+    }
+
+
+    private fun inactivePickP1() {
+        _binding?.playerBtnBatu?.setColorFilter(Color.LTGRAY)
+        _binding?.playerBtnGunting?.setColorFilter(Color.LTGRAY)
+        _binding?.playerBtnKertas?.setColorFilter(Color.LTGRAY)
+    }
+    private fun inactivePickP2() {
         _binding?.comBtnBatu?.setColorFilter(Color.LTGRAY)
         _binding?.comBtnGunting?.setColorFilter(Color.LTGRAY)
         _binding?.comBtnKertas?.setColorFilter(Color.LTGRAY)
     }
 
-    private fun refreshPick() {
+    private fun refreshPickP1() {
         _binding?.btnLockPick?.isVisible = false
         _binding?.playerBtnBatu?.setBackgroundColor(Color.TRANSPARENT)
         _binding?.playerBtnBatu?.colorFilter = null
-        _binding?.comBtnBatu?.setBackgroundColor(Color.TRANSPARENT)
-        _binding?.comBtnBatu?.colorFilter = null
         _binding?.playerBtnGunting?.setBackgroundColor(Color.TRANSPARENT)
         _binding?.playerBtnGunting?.colorFilter = null
-        _binding?.comBtnGunting?.setBackgroundColor(Color.TRANSPARENT)
-        _binding?.comBtnGunting?.colorFilter = null
         _binding?.playerBtnKertas?.setBackgroundColor(Color.TRANSPARENT)
         _binding?.playerBtnKertas?.colorFilter = null
+        _binding?.textTerminal?.setTextColor(Color.BLACK)
+    }
+    private fun refreshPickP2() {
+        _binding?.btnLockPick2?.isVisible = false
+        _binding?.comBtnBatu?.setBackgroundColor(Color.TRANSPARENT)
+        _binding?.comBtnBatu?.colorFilter = null
+        _binding?.comBtnGunting?.setBackgroundColor(Color.TRANSPARENT)
+        _binding?.comBtnGunting?.colorFilter = null
         _binding?.comBtnKertas?.setBackgroundColor(Color.TRANSPARENT)
         _binding?.comBtnKertas?.colorFilter = null
         _binding?.textTerminal?.setTextColor(Color.BLACK)
     }
 
-    private fun disablePick() {
+    private fun disablePickP1() {
         _binding?.playerBtnBatu?.isClickable = false
         _binding?.playerBtnGunting?.isClickable = false
         _binding?.playerBtnKertas?.isClickable = false
         _binding?.btnLockPick?.isVisible = false
     }
+    private fun disablePickP2() {
+        _binding?.comBtnBatu?.isClickable = false
+        _binding?.comBtnGunting?.isClickable = false
+        _binding?.comBtnKertas?.isClickable = false
+        _binding?.btnLockPick2?.isVisible = false
+    }
 
-    private fun enablePick() {
+    private fun enablePickP1() {
         _binding?.playerBtnBatu?.isClickable = true
         _binding?.playerBtnGunting?.isClickable = true
         _binding?.playerBtnKertas?.isClickable = true
     }
+    private fun enablePickP2() {
+        _binding?.comBtnBatu?.isClickable = true
+        _binding?.comBtnGunting?.isClickable = true
+        _binding?.comBtnKertas?.isClickable = true
+    }
 
-    fun comPickReveal() {
+    fun pickRevealP1() {
+        // 0: Batu      1: Gunting      2:Kertas
+        when (pemain1) {
+            pilihanSuit[0] -> {
+                _binding?.playerBtnBatu?.setBackgroundColor(resources.getColor(R.color.active))
+                _binding?.playerBtnBatu?.colorFilter = null
+            }
+            pilihanSuit[1] -> {
+                _binding?.playerBtnGunting?.setBackgroundColor(resources.getColor(R.color.active))
+                _binding?.playerBtnGunting?.colorFilter = null
+            }
+            else -> {
+                _binding?.playerBtnKertas?.setBackgroundColor(resources.getColor(R.color.active))
+                _binding?.playerBtnKertas?.colorFilter = null
+            }
+        }
+    }
+
+    fun pickRevealP2() {
         // 0: Batu      1: Gunting      2:Kertas
         when (pemain2) {
             pilihanSuit[0] -> {
@@ -159,11 +279,11 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun terminalText() {
-        when (status) {
+        when (status){
             "Pemain 1 MENANG!" -> {
                 _binding?.textTerminal?.setTextColor(resources.getColor(R.color.player_win))
             }
-            "COM MENANG!" -> {
+            "Pemain 2 MENANG!" -> {
                 _binding?.textTerminal?.setTextColor(resources.getColor(R.color.com_win))
             }
             else -> {
@@ -172,8 +292,8 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    private fun playTerminal() {
-        inactivePickCom()
+    private fun playTerminalP1mode() {
+        inactivePickP2()
         val count: CountDownTimer = object : CountDownTimer(3000, 1000) {
             override fun onTick(millisUntilFinished: Long) {
                 _binding?.textTerminal?.text = ((millisUntilFinished / 1000) + 1).toString()
@@ -184,15 +304,38 @@ class MainActivity : AppCompatActivity() {
 
             override fun onFinish() {
                 _binding?.textTerminal?.text = status
-                comPickReveal()
+                pickRevealP2()
                 terminalText()
-                Log.d("Pemain COM Input", "$pemain2")
+                Log.d("Pemain 2 Input", "$pemain2")
                 Log.d("Hasil pertandingan,", "$status")
                 _binding?.btnRefresh?.isClickable = true
                 _binding?.btnRefresh?.isVisible = true
             }
         }
         count.start()
-
     }
+    private fun playTerminalP2mode() {
+
+        val count: CountDownTimer = object : CountDownTimer(3000, 1000) {
+            override fun onTick(millisUntilFinished: Long) {
+                _binding?.textTerminal?.text = ((millisUntilFinished / 1000) + 1).toString()
+                Log.d("Countdown", "playTerminal: ${((millisUntilFinished / 1000)) + 1}")
+                _binding?.btnRefresh?.isClickable = false
+                _binding?.btnRefresh?.isVisible = false
+            }
+
+            override fun onFinish() {
+                _binding?.textTerminal?.text = status
+                pickRevealP2()
+                pickRevealP1()
+                terminalText()
+                Log.d("Pemain 2 Input", "$pemain2")
+                Log.d("Hasil pertandingan,", "$status")
+                _binding?.btnRefresh?.isClickable = true
+                _binding?.btnRefresh?.isVisible = true
+            }
+        }
+        count.start()
+    }
+
 }
